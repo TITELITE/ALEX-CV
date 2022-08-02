14749628054514b81d154d6a929968c07154341d
ALEX <acasetraffic@gmail.com>
2022-08-02 17:36:51 +0200

    error bot

--- script/telegram.php
+++ script/telegram.php
@@ -20,7 +20,7 @@ foreach($arr as $key => $value) {
     $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMassage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
     header('Location: thank_you.html');
