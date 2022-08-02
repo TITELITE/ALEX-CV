454eba2d441a37b4e22f07f1a9019b04b8315518
ALEX <acasetraffic@gmail.com>
2022-08-02 22:37:42 +0200

    test

--- script/telegram.php
+++ script/telegram.php
@@ -5,30 +5,26 @@ XXXXXXXXXXXXXXXXXXXXXXX - токен бота*/
 
 $name = $_POST['user_name'];
 $email = $_POST['user_email'];
-$text = $_POST['user_text'];
+
 
 $token = "5428210587:AAHPtWmbxD2WAlMe56zXCryAaHljiSJ6agQ";
-$chat_id ="-613203752";
-
+$chat_id = "-613203752";
 $arr = array(
-    'Name: ' => $name,
-    'Email: ' => $email,
-    'Text: ' => $text
+  'Имя пользователя: ' => $name,
+  'Email: ' => $email
 );
 
 foreach($arr as $key => $value) {
-    $txt .= "<b>".$key."</b> ".$value."%0A";
+  $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-    header('Location: thank_you.html');
-    var_dump($sendToTelegram);
-die;
-
+  header('Location: thank-you.html');
+  
 } else {
-    echo "Error";
+  echo "Error";
 }
 
 ?>
